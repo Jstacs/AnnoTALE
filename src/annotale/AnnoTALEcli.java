6ebaf180cf34fd27ab1953d8fe926b7c869e1b3d
lemora <lea.morschel@informatik.uni-halle.de>
2025-10-28 14:08:59 +0100

    Migrate Annotale sources to annotale namespace

---
+++ src/annotale/AnnoTALEcli.java
@@ -0,0 +1,41 @@
+package annotale;
+import de.jstacs.tools.JstacsTool;
+import de.jstacs.tools.ui.cli.CLI;
+import projects.tals.prediction.QuickTBSPredictionTool;
+import projects.tals.rnaseq.DerTALE;
+import annotale.tools.ClassAssignmentTool;
+import annotale.tools.ClassBuilderTool;
+import annotale.tools.ClassPresenceTool;
+import annotale.tools.LoadAndViewClassesTool;
+import annotale.tools.PredictAndIntersectTargetsTool;
+import annotale.tools.RenameTool;
+import annotale.tools.TALEAnalysisTool;
+import annotale.tools.TALEComparisonTool;
+import annotale.tools.TALEPredictionTool;
+
+
+
+public class AnnoTALEcli {
+
+	public static void main( String[] args ) throws Exception {
+		
+		JstacsTool[] tools = new JstacsTool[]{
+				new TALEPredictionTool(),
+				new TALEAnalysisTool(), 
+				new ClassBuilderTool(), 
+				new LoadAndViewClassesTool(), 
+				new ClassAssignmentTool(), 
+				new RenameTool(), 
+				new PredictAndIntersectTargetsTool(),
+                new ClassPresenceTool(),
+                new TALEComparisonTool(),
+                new QuickTBSPredictionTool(),
+                new DerTALE()};
+
+		CLI cli = new CLI( tools );
+		
+		cli.run( args );
+
+	}
+
+}
